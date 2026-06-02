b126c07d019273e224838a08d0eb52047f998eb9
phantomray02 <phantomray02@gmail.com>
2026-06-02 16:51:30 -0500

    Update app.py

--- pdp-qa-tool/app.py
+++ pdp-qa-tool/app.py
@@ -41,19 +41,22 @@ IMAGE_ORDER = [
 html_cache = {}
 image_cache = {}
 
-import requests
-import re
+
+from playwright.sync_api import sync_playwright
 
 def get_html(url):
-    headers = {
-        "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/120.0.0.0 Safari/537.36"
-        ),
-        "Accept": "application/json, text/html, */*",
-        "Referer": "https://www.cvs.com/",
-    }
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        page.goto(url)
+        page.wait_for_load_state("networkidle")
+
+        html = page.content()
+        browser.close()
+
+    return html
+
 
     # =========================================
     # ✅ STEP 1: TRY API USING PRODUCT ID
