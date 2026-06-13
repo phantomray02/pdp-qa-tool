109d2017a9cbb3796e21314b517996338f237bae
phantomray02 <phantomray02@gmail.com>
2026-06-12 22:32:43 -0500

    Update app.py

--- pdp-qa-tool/app.py
+++ pdp-qa-tool/app.py
@@ -88,8 +88,8 @@ thread_local = threading.local()
 # =========================================
 # VISUAL LAYOUT SETTINGS
 # =========================================
-SECTION_HEADER_SIZE = 75
-COPY_TEXT_SIZE = 45
+SECTION_HEADER_SIZE = 50
+COPY_TEXT_SIZE = 30
 COPY_LINE_HEIGHT = 1.20
 SECTION_VERTICAL_GAP = 15
 
