9b327d8282e49a0987ee2201b2c3b9a24018004b
phantomray02 <phantomray02@gmail.com>
2026-06-19 19:10:39 -0500

    Update app.py

--- pdp-qa-tool/app.py
+++ pdp-qa-tool/app.py
@@ -6312,7 +6312,7 @@ if (
                 clean_rpc = clean_item_number(raw_rpc)
 
                 top_mid.markdown(
-                    f"<div style='margin-left:220px;'>" + column_header_link_html(
+                    f"<div style='margin-left:180px;'>" + column_header_link_html(
                         retailer_name,
                         clean_rpc,
                         retail_url,
